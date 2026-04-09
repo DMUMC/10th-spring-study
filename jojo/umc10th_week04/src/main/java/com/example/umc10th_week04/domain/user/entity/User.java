969f9a6ff2e84267b47844a251eae3ffb8196f9f
Junhyuk K <kjh27256901@gmail.com>
2026-04-09 20:20:45 +0900

    chore : ERD-based domain design & Build setup

---
+++ jojo/umc10th_week04/src/main/java/com/example/umc10th_week04/domain/user/entity/User.java
@@ -0,0 +1,4 @@
+package com.example.umc10th_week04.domain.user.entity;
+
+public class User {
+}
---
+++ jojo/umc10th_week04/src/main/java/com/example/umc10th_week04/domain/user/repository/UserFavorFoodRepository.java
@@ -0,0 +1,4 @@
+package com.example.umc10th_week04.domain.user.repository;
+
+public interface UserFavorFoodRepository {
+}
